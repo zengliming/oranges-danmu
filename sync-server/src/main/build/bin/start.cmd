eddf5e39acddaf80c56a9da6857d0ba2c534c4b7
zmisme <2604609656@qq.com>
2019-05-28 00:35:12 +0800

    添加启动和停止脚本

--- sync-server/src/main/build/bin/start.cmd
+++ sync-server/src/main/build/bin/start.cmd
@@ -1,5 +1,12 @@
 @echo off
 chcp 936
 call cd ../
-call java -Xms800m -Xmx800m -XX:MaxNewSize=512m -jar sync-server-1.0-SNAPSHOT.jar port=8088
+rem 获取当前路径
+set dir=%cd%
+rem 凭借命令
+set cmds=java -Xms800m -Xmx800m -XX:MaxNewSize=512m -jar
+set port=port=8088
+set filename=sync-server-1.0-SNAPSHOT.jar
+set result=%cmds% %dir%\%filename% %port%
+call %result%
 cmd.exe
