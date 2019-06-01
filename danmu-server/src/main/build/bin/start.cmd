304ef08f5d378253351c559e00ecde34f798f6ae
zlmisme <2604609656@qq.com>
2019-06-01 16:07:30 +0800

    修改脚本

--- danmu-server/src/main/build/bin/start.cmd
+++ danmu-server/src/main/build/bin/start.cmd
@@ -6,7 +6,7 @@ set dir=%cd%
 rem 凭借命令
 set cmds=java -Xms800m -Xmx800m -XX:MaxNewSize=512m -jar
 set port=9000
-set filename=sync-server-1.0-SNAPSHOT.jar
+set filename=danmu-server-1.0-SNAPSHOT.jar
 set result=%cmds% %dir%\%filename% %port%
 call %result%
 cmd.exe
